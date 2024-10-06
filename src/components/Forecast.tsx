a01b8b28324207e021962a1cb4125131e23b532e
Matt <mphlamb@gmail.com>
2024-10-06 10:42:46 +0200

    temp and description added

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,18 +1,28 @@
 import { ForecastType } from "../types";
 
 type ForecastProps = {
-    data: ForecastType
-}
+	data: ForecastType;
+};
 
 function Forecast({ data }: ForecastProps): JSX.Element {
-    console.log(data);
-    
+	console.log(data);
+
 	return (
 		<>
-			<section>
-				<h1>Forecast:</h1>
-				{/* <p>{data.sunrise}</p> */}
-			</section>
+			<div className="flex w-full md:max-w-[500px] py-4 md:py-4 md:px-10 lg:px-24 h-full lg:h-auto justify-center">
+				<section className="text-center">
+					<h1 className="text-2xl font-black">{data.city.name}</h1>
+					<h2 className="text-4xl">
+						{Math.round(data.list[0].main.temp)}
+						<span className="font-light">℃</span>
+					</h2>
+                    <p>{data.list[0].weather[0].description}</p>
+                    <p className="flex gap-4">
+                        <span className="flex gap-4">Min {Math.floor(data.list[0].main.temp)}℃</span>
+                        <span className="flex gap-4">Max {Math.ceil(data.list[0].main.temp)}℃</span>
+                    </p>
+				</section>
+			</div>
 		</>
 	);
 }
